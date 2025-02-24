855f343454fa5fa6468965da14bba154db669083
zinine73 <zinine@naver.com>
2025-02-24 15:51:06 +0900

    character

--- Assets/3.Scripts/BoardManager.cs
+++ Assets/3.Scripts/BoardManager.cs
@@ -6,23 +6,64 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Passable;
+    }
+
+    private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
+    private Grid m_Grid;
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
+    public Tile[] WallTiles;
+    public PlayerController Player;
 
     void Start()
     {
+        m_Grid = GetComponentInChildren<Grid>();
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_BoardData = new CellData[Width, Height];
 
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width; x++)
             {
-                int tileNumber = Random.Range(0, GroundTiles.Length);
-                m_Tilemap.SetTile(new Vector3Int(x, y, 0), GroundTiles[tileNumber]);
+                Tile tile;
+                m_BoardData[x, y] = new CellData();
+                if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
+                {
+                    // Wall tile
+                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
+                }
+                else
+                {
+                    // ground tile
+                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
+                }
+                m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+        Player.Spawn(this, new Vector2Int(1, 1));
+    }
+
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+            || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        return m_BoardData[cellIndex.x, cellIndex.y];
     }
 }
---
+++ Assets/3.Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private BoardManager m_Board;
+    private Vector2Int m_CellPosition;
+
+    public void Spawn(BoardManager boardManager, Vector2Int cell)
+    {
+        m_Board = boardManager;
+        MoveTo(cell);
+    }
+
+    public void MoveTo(Vector2Int cell)
+    {
+        m_CellPosition = cell;
+
+        // 보드에서의 player위치 지정 => 화면에서 제대로된 위치에 표시
+        transform.position = m_Board.CellToWorld(m_CellPosition);
+    }
+
+    private void Update()
+    {
+        Vector2Int newCellTarget = m_CellPosition;
+        bool hasMoved = false;
+
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.y++;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.y--;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.x++;
+            hasMoved = true;
+        }
+        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            newCellTarget.x--;
+            hasMoved = true;
+        }
+
+        if (hasMoved)
+        {
+            //셀이 움직일 수 있으면 움직여라
+            BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
+
+            if (cellData != null && cellData.Passable)
+            {
+                MoveTo(newCellTarget);
+            }
+        }
+    }
+}
